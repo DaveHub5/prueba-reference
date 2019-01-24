5a38752a2857b39c96b28c94f6a15098ac5536cb
David <davidperal96@gmail.com>
2019-01-24 18:55:39 +0100

    Update aux.js

--- src/aux.js
+++ src/aux.js
@@ -4,106 +4,192 @@ Datagrid, TextField, ReferenceField, EditButton,
 AutocompleteInput, required, FormDataConsumer,
 ReferenceArrayInput, SelectArrayInput,
  } from 'react-admin';
+import Autosuggest from 'react';
 
 
-  class Post extends React.Component{
-   constructor(props) { 
-    super(props);
-    this.id = this.props.id;
-    this.title = this.props.title;
-    this.author = this.props.author;
-    this.aux = this.props.aux;
-  }
-    
-    render() {
-    return (
-      <div>
-        <h4>{this.id}  {this.title}  {this.author}  {this.aux}</h4>       
-      </div>
-    );
-  }  
-  }
-  
-  class Comment extends React.Component{
-   constructor(props) { 
-    super(props);
-    this.postId = this.props.postId;
-    this.id = this.props.id;
-    this.body = this.props.body;
-  }
-    
-    render() {
-    return (
-      <div>
-        <h4>{this.postId} {this.id} {this.body}</h4>       
-      </div>
-    );
-  }  
-  } 
-  /*
-  function DoSomething(){
-    return (
-      <div>
-  <Post id={id} title={title} author={author} aux={aux}/>
-  <Comment postId={id} id={commentId} body={body}/>
-     </div>);
 
-  }*/
-  function Prueba(posts, comments){
-   const i = 0;
-   const j = 0;
-    for (i=0; i<posts.posts.length; i++){
-      console.log("DD" + i)
-      for (j=0; j<posts.comments.length; j++) {
-        if(posts.comments[j].postId == posts.posts[i].id){
-       console.log(posts.comments[j].body)     
-        }
+const languages = [
+  {
+    title: '1970s',
+    languages: [
+      {
+        name: 'C',
+        year: 1972
       }
-    }
-    
-    
-    return (
-     <div>
- <Post id={posts.posts[1].id} title={posts.posts[1].title} author={posts.posts[1].author} aux={posts.posts[1].aux}/>        
-     </div>
-    );
-  }
-  
-  
-    function PruebaSelect(posts, comments, selected){
-      const aux = [];
-	var j = 0; 
-      for (j=0; j<posts.comments.length; j++) {
-        if(posts.comments[j].postId == posts.selected){  
-          aux.push(posts.comments[j]);
-        }
+    ]
+  },
+  {
+    title: '1980s',
+    languages: [
+      {
+        name: 'C++',
+        year: 1983
+      },
+      {
+        name: 'Perl',
+        year: 1987
       }
-      console.log(aux)
-    return (
-    <div>
-	Hi
-    </div>
-    );
-
+    ]
+  },
+  {
+    title: '1990s',
+    languages: [
+      {
+        name: 'Haskell',
+        year: 1990
+      },
+      {
+        name: 'Python',
+        year: 1991
+      },
+      {
+        name: 'Java',
+        year: 1995
+      },
+      {
+        name: 'Javascript',
+        year: 1995
+      },
+      {
+        name: 'PHP',
+        year: 1995
+      },
+      {
+        name: 'Ruby',
+        year: 1995
+      }
+    ]
+  },
+  {
+    title: '2000s',
+    languages: [
+      {
+        name: 'C#',
+        year: 2000
+      },
+      {
+        name: 'Scala',
+        year: 2003
+      },
+      {
+        name: 'Clojure',
+        year: 2007
+      },
+      {
+        name: 'Go',
+        year: 2009
+      }
+    ]
+  },
+  {
+    title: '2010s',
+    languages: [
+      {
+        name: 'Elm',
+        year: 2012
+      }
+    ]
   }
+];
+
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
   
-  const posts = [
-    { id: 1, title: "HawkEye", author: "Gorbien", aux: ["a", "b", "c"]},
-    { id: 2, title: "HawkEye", author: "Gorbien", aux: ["a", "b", "c"]},
-    { id: 3, title: "HawkEye", author: "Gorbien", aux: ["a", "b", "c"]}
-                ];
-  const comments = [
-    { postId: 1, id: 1, body: "YES"},
-    { postId: 1, id: 2, body: "YES"},
-    { postId: 2, id: 3, body: "NO"}
-                ];
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages
+    .map(section => {
+      return {
+        title: section.title,
+        languages: section.languages.filter(language => regex.test(language.name))
+      };
+    })
+    .filter(section => section.languages.length > 0);
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+function renderSectionTitle(section) {
+  return (
+    <strong>{section.title}</strong>
+  );
+}
+
+function getSectionSuggestions(section) {
+  return section.languages;
+}
+
+class App2 extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };    
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest 
+        multiSection={true}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        renderSectionTitle={renderSectionTitle}
+        getSectionSuggestions={getSectionSuggestions}
+        inputProps={inputProps} />
+    );
+  }
+}
+
 
 export const Aux = props => (
-<SimpleForm>
-<ReferenceInput label="Post" source="postId" reference="posts">
-    <AutocompleteInput optionText="id"/>
-</ReferenceInput>
-  <PruebaSelect posts="posts" comments="comments" selected={1}/>
 
-</SimpleForm>
+<App2 />
+
 );
