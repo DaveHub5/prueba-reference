df980c92d9cbb8142208e74ab0f4d9606099f8f5
David <davidperal96@gmail.com>
2019-02-04 11:53:26 +0100

    With Refresh

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { Prueba } from './prueba';
+import { Prueba2 } from './prueba2';
 
 const dataProvider = jsonServerProvider('http://localhost:3001');
 const App = () => <Admin dataProvider={dataProvider}>
         <Resource name="posts" list={ListGuesser} />
         <Resource name="comments" list={ListGuesser} /> 
         <Resource name="prueba" list={Prueba} />
+        <Resource name="prueba2" list={Prueba2} />
 	</Admin>
 
 export default App;
