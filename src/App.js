ebe458e57edc7a4a97645e40491316730c16a738
Fernando Navarro <fnp.formacion@gmail.com>
2019-02-04 09:25:11 +0100

    Cambios no guardado en la rama master

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { Prueba } from './prueba';
-import { Formp  } from './formp';
-import { Aux    } from './aux';
 
 const dataProvider = jsonServerProvider('http://localhost:3001');
 const App = () => <Admin dataProvider={dataProvider}>
         <Resource name="posts" list={ListGuesser} />
         <Resource name="comments" list={ListGuesser} /> 
         <Resource name="prueba" list={Prueba} />
-	<Resource name="formp" list={Formp} />
-	<Resource name="aux"   list={Aux} />
 	</Admin>
 
 export default App;
--- src/prueba.js
+++ src/prueba.js
@@ -2,28 +2,30 @@ import React from 'react';
 import { List, SimpleForm, ReferenceInput, SelectInput,
 Datagrid, TextField, ReferenceField, EditButton,
 AutocompleteInput, required, FormDataConsumer,
-ReferenceArrayInput, SelectArrayInput,
+ReferenceArrayInput, SelectArrayInput, Show,
  } from 'react-admin';
 
-/*
-			<ReferenceInput label="Aux" source="postId" reference="posts">
-				<ReferenceInput label="Aux" source="postId" reference="comments">
-    				<AutocompleteInput optionText="body"/>
-				</ReferenceInput>
-			</ReferenceInput>
-*/
-const optionRenderer = choice => `"$posts.id" $"posts.body"`;
 
 
 
 export const Prueba = props => (
+
+
 <SimpleForm>
-<ReferenceInput label="Post" source="postId" reference="posts">
+<ReferenceInput label="Posts" source="postId" reference="posts">
     <AutocompleteInput optionText="id"/>
 </ReferenceInput>
-	<ReferenceInput label="Aux" source="postId" reference="comments">
-    		<AutocompleteInput optionText="id" />
+
+<FormDataConsumer>
+   {({ formData, ...rest }) =>
+	<ReferenceInput label="Comments" source="id" reference="comments" filter={{ postId: formData.postId }}>
+    		<AutocompleteInput optionText="body"/>
 	</ReferenceInput>
+   }
+</FormDataConsumer>
 
 </SimpleForm>
-);
+
+	)
+
+
