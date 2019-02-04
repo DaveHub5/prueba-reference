b51a8e68ac648cbc9c11ab16fb8ff0b23abd140b
David <davidperal96@gmail.com>
2019-02-04 10:25:55 +0100

    commit

--- src/date.js
+++ src/date.js
@@ -2,28 +2,45 @@ import React from 'react';
 import { List, SimpleForm, ReferenceInput, SelectInput,
 Datagrid, TextField, ReferenceField, EditButton,
 AutocompleteInput, required, FormDataConsumer,
-ReferenceArrayInput, SelectArrayInput, Show, DateField, 
+ReferenceArrayInput, SelectArrayInput, Show, DateField,
  } from 'react-admin';
 
 import { DateInput, TimeInput, DateTimeInput } from 'react-admin-date-inputs';
 
+  class NewDate extends React.Component{
+   
+   constructor(props) { 
+    super(props);
+    this.state = {date: new Date('2017-04-23')};  
+    this.handleDate = this.handleDate.bind(this); 
+  }
+handleDate(){
+    this.setState({
+      date: new Date()
+    });
+}
 
-export const Date = props => (
 
-
+render(){
+return(
 <SimpleForm>
-        <DateField label="Publication date" source="startDate" />
+       <DateField label="Publication date" source={this.state.date} />
 	<DateInput source="startDate" />
 	<DateTimeInput source="published_at" />
 <h1> HH </h1>
 
-        <DateInput source="startDate" label="Start date" options={{ format: 'dd/MM/yyyy',  
-awareOfUnicodeTokens: true,   useAdditionalDayOfYearTokens: true, useAdditionalWeekYearTokens: true 
-}} />
+        <DateInput source="startDate" label="Start date" options={{ format: 'dd/MM/yyyy'}} />
         <TimeInput source="startTime" label="Start time" options={{ format: 'HH:mm:ss' }} />
         <DateTimeInput source="endDate" label="End time" options={{ format: 'dd/MM/yyyy, HH:mm:ss', ampm: false, clearable: true }} />
 
-
 </SimpleForm>
 
+)
+}
+
+
+}//end of class
+
+export const Date = props => (
+	<NewDate />
 	)
